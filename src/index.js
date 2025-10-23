2697f0988cb657310dfb148aea309c6ed41d79a5
Podexus23 <santon23@gmail.com>
2025-10-23 23:24:44 +0300

    refactor: cors logic

--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 require("dotenv").config();
 const http = require("http");
 const LightService = require("./service/light.service");
-const SupabaseService = require("./service/supabase.service");
 const mainRouter = require("./routes/index");
+const corsHelper = require("./helpers/cors.helper");
 
 const PORT = process.env.PORT || 5050;
 
 LightService.startGenerateLight();
 
 const app = http.createServer(async (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  // Обработка preflight-запросов
-  if (req.method === "OPTIONS") {
-    res.writeHead(204);
-    res.end();
-    return;
-  }
+  corsHelper(req, res);
 
   mainRouter(req, res);
 });
